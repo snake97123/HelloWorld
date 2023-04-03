867496982e55d81228f47b7e0ac6b77d47d2a650
snake97123 <snake12379@yahoo.co.jp>
2023-04-03 16:34:19 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Wataru Yamashita on 2023/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
